3899c10447204357395fa97c016ed7fe247f07d4
Sergio <sergioruza22@gmail.com>
2022-10-24 17:30:12 -0300

    Realizando cosntrução da pagina de login

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <Router>
-      <App />
-    </Router>,
+    <App />,
   );
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MyContext from '../context/MyContext';
 
 export default function Login() {
+  const {
+    handleChangeEmail,
+    handleChangePassWord,
+    email,
+    password,
+  } = useContext(MyContext);
+
+  const handleDisableBtn = () => {
+    const regex = /\S+@\S+\.\S+/;
+    const passLength = 7;
+    const verifyEmail = email && regex.test(email);
+    const verifyName = password.length === passLength;
+    return (verifyEmail && verifyName);
+  };
+  console.log(handleDisableBtn());
+  const onClickBtnLogin = () => {
+    // aaaa
+  };
+
   return (
-    <div>Login</div>
+    <form>
+      Login
+      <input
+        onChange={ handleChangeEmail }
+        type="text"
+        value={ email }
+        data-testid="email-input"
+      />
+      <input
+        onChange={ handleChangePassWord }
+        value={ password }
+        type="password"
+        data-testid="password-input"
+      />
+      <button
+        onClick={ onClickBtnLogin }
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ !handleDisableBtn() }
+      >
+        Enter
+      </button>
+    </form>
   );
 }
