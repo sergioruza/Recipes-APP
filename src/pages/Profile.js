ea38ad6ee0c0b7703a0d88d99a1fe6720fa19be5
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-10-24 19:22:52 -0300

    Página de Profile e seus testes cobertos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,47 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
-function Profile() {
+function Profile(props) {
+  const getUser = JSON.parse(localStorage.getItem('user'));
+
+  const handleLogout = () => {
+    const { history } = props;
+    localStorage.setItem('user', null);
+    history.push('/');
+  };
+
   return (
     <div>
       Profile
+      <p
+        data-testid="profile-email"
+      >
+        {getUser ? getUser.email : 'Email não encontrado'}
+      </p>
+      <Link to="/done-recipes">
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      </Link>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleLogout }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Profile;
