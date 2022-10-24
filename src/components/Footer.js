7ba04d48bbff4ac1c885ae1b826af968d1d45a90
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-10-24 17:49:50 -0300

    feat: R16, R17 e R18 passando tudo show, firma

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import mealIcon from '../images/mealIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer data-testid="footer">
+      <Link to="/meals">
+        <img src={ mealIcon } alt="meal icon" data-testid="meals-bottom-btn" />
+      </Link>
+      <Link to="/drinks">
+        <img src={ drinkIcon } alt="drink icon" data-testid="drinks-bottom-btn" />
+      </Link>
+    </footer>
   );
 }
 
