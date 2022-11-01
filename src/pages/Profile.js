d5cefc98c7c678b3082c8f4697ea5329f99707cc
unknown <sergioruza22@gmail.com>
2022-11-01 15:07:52 -0300

    seila

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Typography } from '@mui/material';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipeProvider from '../context/RecipeProvider';
+import '../css/Profile.css';
 
 function Profile(props) {
   const getUser = JSON.parse(localStorage.getItem('user'));
@@ -24,21 +26,43 @@ function Profile(props) {
       <p
         data-testid="profile-email"
       >
-        {getUser ? getUser.email : 'Email não encontrado'}
+        {getUser ? <Typography className='email-profile' variant='h4'>{ getUser.email }</Typography> : 'Email não encontrado'}
       </p>
-      <Link to="/done-recipes">
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      </Link>
-      <Link to="/favorite-recipes">
-        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      </Link>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ handleLogout }
-      >
-        Logout
-      </button>
+
+      <div className='btns-profile'>
+        <Link to="/done-recipes">
+          <Button
+            variant="contained"
+            size="medium"
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+
+          </Button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <Button
+            variant="contained"
+            size="medium"
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+
+          </Button>
+        </Link>
+        </div>
+        <Button
+        className='btn-logout'
+          variant="contained"
+          size="medium"
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLogout }
+        >
+          Logout
+        </Button>
       <Footer />
     </div>
   );
